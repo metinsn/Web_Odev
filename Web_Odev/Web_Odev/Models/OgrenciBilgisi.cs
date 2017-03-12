ac5b0e93107f78bd28b0b20d3eb71a409b4b926d
metin.sen <metin.sen@ALS-YG39506-J06>
2017-03-12 18:42:07 +0300

    1

---
+++ Web_Odev/Web_Odev/Models/OgrenciBilgisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Odev.Models
+{
+    public class OgrenciBilgisi
+    {
+        public int OgrID { get; set; }
+        public int OgrAdi { get; set; }
+        public int OgrSoyadi { get; set; }
+        public int OgrNo { get; set; }
+        public int  OgrKursID { get; set; }
+        public int OgrKurs { get; set; }
+    }
+}
